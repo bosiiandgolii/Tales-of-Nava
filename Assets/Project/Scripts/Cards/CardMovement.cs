2d7c7dee29e352272e01b926f4170c582f7f2c93
NurePoturaikoNazar <nazar.poturaiko@nure.ua>
2026-07-06 19:39:01 +0200

    backup

--- Assets/Project/Scripts/Cards/CardMovement.cs
+++ Assets/Project/Scripts/Cards/CardMovement.cs
@@ -46,17 +46,14 @@ public class CardMovement : MonoBehaviour, IDragHandler, IPointerDownHandler, IP
                 }
                 break;
             case 3:
-                HandlePlayState();
-                if (!Input.GetMouseButton(0))
-                {
-                    TransitionToState0();
-                }
+                HandlePlayState();         
                 break;
         }
     }
 
     private void TransitionToState0()
     {
+        Debug.Log("Using method TransitionToState0. => State 0");
         currentState = 0;
         rectTransform.localScale = originalScale;
         rectTransform.localRotation = originalRotation;
@@ -69,28 +66,34 @@ public class CardMovement : MonoBehaviour, IDragHandler, IPointerDownHandler, IP
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        Debug.Log("Pointer Entered");
         if (currentState == 0)
         {
             originalPosition = rectTransform.localPosition;
             originalRotation = rectTransform.localRotation;
             originalScale = rectTransform.localScale;
 
+            Debug.Log("Pointer Entered. State 0 => State 1");
             currentState = 1;
         }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        Debug.Log("Pointer Exit. State: " + currentState);
         if (currentState == 1)
         {
+            Debug.Log("Pointer Exit. State 1 => State 0");
             TransitionToState0();
         }
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
+        Debug.Log("Pointer Down" + currentState);
         if (currentState == 1)
         {
+            Debug.Log("Start dragging. State 1 => State 2");
             currentState = 2;
             RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(), eventData.position, eventData.pressEventCamera, out originalLocalPointerPosition);
 
@@ -100,17 +103,21 @@ public class CardMovement : MonoBehaviour, IDragHandler, IPointerDownHandler, IP
 
     public void OnDrag(PointerEventData eventData)
     {
+        Debug.Log("Dragging. State: " + currentState);
+        
         if (currentState == 2)
         {
-            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(), eventData.position, eventData.pressEventCamera, out Vector2 localPointerPosition))
+            Vector2 localPointerPosition;
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(), eventData.position, eventData.pressEventCamera, out localPointerPosition))
             {
-                localPointerPosition /= canvas.scaleFactor;
+                rectTransform.position = Input.mousePosition;
+                
+                Debug.Log("Mouse: " + Input.mousePosition);
+                Debug.Log("Card Pos: " + rectTransform.localPosition);
 
-                Vector2 offsetToOriginal = localPointerPosition - originalLocalPointerPosition;
-                rectTransform.localPosition = originalPanelLocalPositon + (Vector3)offsetToOriginal;
-
-                if(rectTransform.localPosition.y > cardPlay.y)
+                if (rectTransform.localPosition.y > cardPlay.y)
                 {
+                    Debug.Log("Above play position. State 2 => State 3");
                     currentState = 3;
                     playArrow.SetActive(true);
                     rectTransform.localPosition = playPosition;
@@ -132,11 +139,14 @@ public class CardMovement : MonoBehaviour, IDragHandler, IPointerDownHandler, IP
 
     private void HandlePlayState()
     {
+        Debug.Log ("Play state active");
+
         rectTransform.localPosition = playPosition;
         rectTransform.localRotation = Quaternion.identity;
 
         if (Input.mousePosition.y < cardPlay.y)
         {
+            Debug.Log("Restoring to drag state 3 => 2");
             currentState = 2;
             playArrow.SetActive(false);
         }
